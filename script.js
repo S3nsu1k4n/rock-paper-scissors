d32be3118e4a5af309a74ad0fe047fdaf86824b0
S3nsu1k4n <saschakoeppe@gmail.com>
2023-07-04 21:14:21 +0900

    finish playRound function

--- script.js
+++ script.js
@@ -8,28 +8,58 @@ function getComputerChoice(){
 function playRound(playerSelection="", computerSelection=""){
     // plays a single round
     // return a string that declares the winner
+    
     let won;
     switch(playerSelection.toLowerCase()){
         case "rock":
             if(computerSelection === "Scissors"){
-                won = true; 
+                won = 1; 
+                
             }
             else if (computerSelection === "Paper"){
-                won = false;
+                won = 0;
+            }
+            else{
+                won = 2;
             }
             break;
         case "paper":
+            if(computerSelection === "Scissors"){
+                won = 0; 
+                
+            }
+            else if (computerSelection === "Rock"){
+                won = 1;
+            }
+            else{
+                won = 2;
+            }
             break;
         case "scissors":
+            if(computerSelection === "Paper"){
+                won = 1; 
+            }
+            else if (computerSelection === "Rock"){
+                won = 0;
+            }
+            else{
+                won = 2;
+            }
             break;
     } 
 
-    if(won){
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    }
+    switch(won){
+        case 0:
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        case 1:
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        case 2:
+            return `It's a tie! ${playerSelection} is ${computerSelection}`;
+        }
     return computerSelection;
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+
 console.log(playRound(playerSelection, computerSelection));
